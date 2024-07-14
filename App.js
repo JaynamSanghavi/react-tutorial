45573f7063840c1dd5cc18f76db2f72f16a02926
Jaynam404 <jaynam.s@somaiya.edu>
2024-07-13 20:53:32 -0500

    Day 1

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading1 = React.createElement("h1", {
+    id: "title",
+}, "Hello World");
+
+const heading2 = React.createElement("h2", {
+    id: "title2",
+}, "FROM REACT");
+
+const divContainer = React.createElement("div", {}, [heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(divContainer);
