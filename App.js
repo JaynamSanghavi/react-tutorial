f14a2ddf89076f7b3bc63f9d9f3af03db1228f26
Jaynam404 <jaynam.s@somaiya.edu>
2024-07-15 16:23:55 -0500

    learned about jsx

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {
-    id: "title",
-}, "Hello World");
 
-const heading2 = React.createElement("h2", {
-    id: "title2",
-}, "FROM REACT");
+//React element
+const JsxHeading = () => (
+    <h1 id="title1" key="jsxtitle">
+        Using JSX
+    </h1>
+);
 
-const divContainer = React.createElement("div", {}, [heading1, heading2]);
+//React component
+// 1. Functional - new | Name should start with capital letter for coding convention
+
+const Header = () => (
+    <div class="header">
+        {/* <JsxHeading /> */}
+        {JsxHeading()}
+        <h1>Hello World</h1>
+        <ul>
+            <li>Home</li>
+            <li>Contact Us</li>
+            <li>Support</li>
+        </ul>
+    </div>
+);
+
+const Header2 = () => (
+    <h2 id="title1" key="jsxtitlecomponent2">Using JSX FUnctional Component 2</h2>
+);
+
+// 2. Class Based - old |
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(divContainer);
+root.render(<Header />);
