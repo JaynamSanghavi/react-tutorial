440c67f1d0116ad48416e3a96320a48cabf9c366
Jaynam404 <jaynam.s@somaiya.edu>
2024-07-20 13:40:14 -0500

    API called and shimmer added

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,29 +1,11 @@
-import { restList, imgCDN } from "./config";
-import {useState} from "react";
+import { useState, useEffect } from "react";
+import RestCard from "./RestCard";
+import Shimmer from "./Shimmer";
 
 
-const RestCard = ({ name, area, cuisines, cloudinaryImageId, aggregatedDiscountInfoV2 }) => {
-    let discountMeta = null;
-    if (aggregatedDiscountInfoV2?.shortDescriptionList)
-        discountMeta = (aggregatedDiscountInfoV2?.descriptionList[0].meta);
-    return (
-        <div className="card">
-            <img
-                src={
-                    imgCDN +
-                    cloudinaryImageId
-                }
-            />
-            <h3>{name}</h3>
-            <h4>{area}</h4>
-            <h4>{cuisines.join(", ")}</h4>
-            {discountMeta && <p>{discountMeta}</p>}
-        </div>
-    );
-};
 
-function filterData(resList, searchText){
-    const filterRestList = resList.filter((res) => res.data.name.includes(searchText));
+function filterData(allRest, searchText) {
+    const filterRestList = allRest.filter((res) => (res.info.name.toLowerCase().includes(searchText.toLowerCase())));
     return filterRestList;
 }
 
@@ -31,22 +13,54 @@ function filterData(resList, searchText){
 
 const BodyComponent = () => {
     const [searchText, setSearchText] = useState("");
-    const [resList, setResList] = useState(restList);
+    const [allRest, setAllRest] = useState([]);
+    const [filterRest, setFilterRest] = useState([]);
+
+
+    // empty dependency array => once after render
+    // dep arry [searchText] => once after initial render + everytime after redern (my searchText changes)
+    useEffect(() => {
+        // API call
+        getRestaurants();
+        console.log("Inside use effect");
+    }, []);
+
+    async function getRestaurants() {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setAllRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilterRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    if (!allRest) return null;
+
+    // not render component (Early return)
+    if(allRest?.length == 0)
+        return <Shimmer/>
+
     return (
+
         <>
             <div className="search-box">
                 <input type="text" value={searchText} placeholder="Enter the key" onChange={(e) => {
                     setSearchText(e.target.value);
-                }}/>
+                }} />
                 <button type="button" onClick={() => {
-                    setResList(filterData(restList,searchText));
+                    setFilterRest(filterData(allRest, searchText));
                 }}>Submit</button>
             </div>
             <div className="card-container">
                 {
-                    resList.map((rest) => {
-                        return <RestCard {...rest.data} key={rest.data.id} />
-                    })
+                    
+                    filterRest?.length > 0 ?
+                        filterRest.map((rest) => {
+                            return <RestCard {...rest.info} key={rest.info.id} />
+                        })
+                    :
+                        <h1>No Find Match</h1>
                 }
             </div>
         </>
---
+++ src/Component/RestCard.js
@@ -0,0 +1,28 @@
+import {imgCDN } from "./config";
+const RestCard = ({ name, areaName, cuisines, cloudinaryImageId, aggregatedDiscountInfoV3 }) => {
+    let discountMeta = null;
+    if (aggregatedDiscountInfoV3?.header){
+        discountMeta = aggregatedDiscountInfoV3?.header;
+        if(aggregatedDiscountInfoV3?.subHeader){
+            discountMeta = discountMeta + " - " + aggregatedDiscountInfoV3?.subHeader;
+        }
+
+    }
+
+    return (
+        <div className="card">
+            <img
+                src={
+                    imgCDN +
+                    cloudinaryImageId
+                }
+            />
+            <h3>{name}</h3>
+            <h4>{areaName}</h4>
+            <h4>{cuisines.join(", ")}</h4>
+            {discountMeta && <p>{discountMeta}</p>}
+        </div>
+    );
+};
+
+export default RestCard;
